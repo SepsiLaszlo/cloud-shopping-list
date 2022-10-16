efd8c84a77badeb4fc6d681c60b7f23245414aa5
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2022-10-16 10:05:47 +0000

    add put item config

--- scripts/create_cloud_list.rb
+++ scripts/create_cloud_list.rb
@@ -29,7 +29,7 @@ item_resource = client.create_resource({
                                          parent_id: root_id,
                                          path_part: "items",
                                        })
-
+# GET ITEMS
 item_method = client.put_method(
   rest_api_id: api.id,
   resource_id: item_resource.id,
@@ -70,5 +70,49 @@ item_integration_response = client.put_integration_response(
     'method.response.header.Access-Control-Allow-Origin': "'*'"
   }
 )
+
+# POST ITEMS
+item_method = client.put_method(
+  rest_api_id: api.id,
+  resource_id: item_resource.id,
+  http_method: 'POST',
+  authorization_type: 'NONE'
+)
+
+item_response = client.put_method_response(
+  rest_api_id: api.id,
+  resource_id: item_resource.id,
+  http_method: 'POST',
+  status_code: '200',
+  response_parameters: {
+    'method.response.header.Access-Control-Allow-Headers': true,
+    'method.response.header.Access-Control-Allow-Origin': true,
+    'method.response.header.Access-Control-Allow-Methods': true
+  },
+)
+
+item_integration = client.put_integration(
+  rest_api_id: api.id,
+  resource_id: item_resource.id,
+  http_method: 'POST',
+  integration_http_method: 'POST',
+  type: 'AWS',
+  uri: "arn:aws:apigateway:#{REGION}:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:596618193278:function:cloud-shopping-list-PutItemFunction-kbDTHW8odAiY/invocations",
+  credentials: 'arn:aws:iam::596618193278:role/ApiGateWayLambdaInvokeRole',
+)
+
+item_integration_response = client.put_integration_response(
+  rest_api_id: api.id,
+  resource_id: item_resource.id,
+  http_method: 'POST',
+  status_code: '200',
+  response_parameters: {
+    'method.response.header.Access-Control-Allow-Headers': "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    'method.response.header.Access-Control-Allow-Methods': "'POST'",
+    'method.response.header.Access-Control-Allow-Origin': "'*'"
+  }
+)
+
+
 p "api #{api.id}"
 p 'DONE'
